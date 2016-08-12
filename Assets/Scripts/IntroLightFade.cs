9b7a334f134686f9e522ca565dcd881ec4dedd40
Mabrie <Markus-Brieden@gmx.de>
2016-08-12 05:02:12 +0200

    Added slowly brightening lights script

---
+++ Assets/Scripts/IntroLightFade.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroLightFade : MonoBehaviour
+{
+
+    private float intensityBefore;
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+    public Light light;
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+    public int fadeDurationInFrames;
+    public int fadeStartsAfterFrames;
+
+    void Start()
+    {
+        setLight();
+        getIntensityBefore();
+        changeIntensity();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("changeIntensity",3.0f);
+    }
+
+    void setLight()
+    {
+        if (Time.frameCount > fadeStartsAfterFrames)
+        {
+            light = gameObject.GetComponent(typeof(Light)) as Light;
+        }
+    }
+
+    Light getLight()
+    {
+        return light;
+    }
+
+
+    void getIntensityBefore()
+    {
+        intensityBefore = getLight().intensity;
+        getLight().intensity = 0.0f;
+    }
+
+    void changeIntensity()
+    {
+        int frame = Time.frameCount;
+        if (getLight().intensity < intensityBefore)
+        {            
+            getLight().intensity = frame * (intensityBefore*0.001f);
+        }
+    }
+}
